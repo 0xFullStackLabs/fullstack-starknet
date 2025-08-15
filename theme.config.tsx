81036f3724511aeec92aab68e478c4e6f61b83ef
Yi Song (Ethan) <ethaninmel@gmail.com>
2025-08-15 15:50:14 +0800

    update

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Fullstack Starknet</span>,
+  project: {
+    link: 'https://github.com/0xFullStackLabs/fullstack-starknet',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/0xFullStackLabs/fullstack-starknet',
+  footer: {
+    text: 'Full Stack Labs',
+  },
+}
+
+export default config
